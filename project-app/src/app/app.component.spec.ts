241ae56292f0532b4ab1717cad452559b6258db8
Mohammed Basel Nasrini <beso_o@live.com>
2020-02-21 10:56:32 +0100

    create project-app components

--- project-app/src/app/app.component.spec.ts
+++ project-app/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Project'`, () => {
+  it(`should have as title 'project-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Project');
+    expect(app.title).toEqual('project-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Project app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('project-app app is running!');
   });
 });
